7da16f8dee60907ed1af41c76eb1264ed74a8250
hey-farhan <inamdarfarhan37@gmail.com>
2025-07-17 01:56:52 +0530

    updated the keys.js to better .env logic

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,4 +1,6 @@
-dbPassword='mongodb+srv://Farhan_DB:farhan123db@farhankacluster.wwmjy.mongodb.net/?retryWrites=true&w=majority&appName=FarhanKaCluster'
+dbPassword='mongodb+srv://DB_USERNAME:DB_PASSWORD@<link to your MongoDB cluster>/?retryWrites=true&w=majority>';
+dbPassword = dbPassword.replace('DB_USERNAME', process.env.DB_USERNAME || 'defaultUsername');
+dbPassword = dbPassword.replace('DB_PASSWORD', process.env.DB_PASSWORD || 'defaultPassword');
 
 module.exports = {
   mongoURI: dbPassword
